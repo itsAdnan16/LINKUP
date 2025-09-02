e0f7b580b2f2ba3d7cbc57569e2ab160425f943d
Adnan <E22CSEU1269@Bennett.edu.in>
2025-09-03 01:57:09 +0530

    everthing is ready for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,41 +1,43 @@
 import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./lib/db.js";
+import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
-//Todo : import the user routes
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chatRoutes.js"
-dotenv.config();
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
+import  connectDB from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 5001;
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
+const PORT = process.env.PORT;
 
-// Middleware should come before routes
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/chat",chatRoutes)
+app.use("/api/chat", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// Basic error handling
-app.use((req, res) => {
-    res.status(404).json({ message: "Route not found" });
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
-  
