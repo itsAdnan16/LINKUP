fa4d6cbb46b3cc6b7bc8968a0fbcee197b49f873
Adnan <E22CSEU1269@Bennett.edu.in>
2025-09-03 00:55:24 +0530

    removed the vercel.json as the team has decided to deploy on render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  base: './',
 })
