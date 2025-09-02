552ea5ff9b60f67d6aa6e81501c7b05e9e352e1a
Adnan <E22CSEU1269@Bennett.edu.in>
2025-09-03 02:26:57 +0530

    changed the default theme from coffee to dark

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("linkup-theme") || "coffee",
+  theme: localStorage.getItem("linkup-theme") || "dark",
   setTheme: (theme) => {
     set({ theme });
     localStorage.setItem("linkup-theme", theme);
